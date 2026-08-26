548f0e19794e95e364c9e4718cdbb35143499029
rezaf28 <RezaFeizi@iran.ir>
2026-08-26 19:30:15 +0330

    Set Version To 2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         minSdk = 24
         targetSdk = 37
         versionCode = 3
-        versionName = "2.0"
+        versionName = "2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
